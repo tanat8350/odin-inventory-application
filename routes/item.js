2d87db7d8f098f05efbc012c8b2cafcba6373b10
tanat8350 <55195958+tanat8350@users.noreply.github.com>
2024-06-23 02:35:03 +0700

    finished item controllers

--- routes/item.js
+++ routes/item.js
@@ -5,11 +5,10 @@ const item_controller = require('../controllers/itemController');
 
 router.get('/', item_controller.list);
 router.get('/create', item_controller.create_get);
-router.post('/item/create', item_controller.create_post);
-router.get('/item/:id/delete', item_controller.delete_get);
-router.post('/item/:id/delete', item_controller.delete_post);
-router.get('/item/:id/update', item_controller.update_get);
-router.post('/item/:id/update', item_controller.update_post);
-router.get('/item/:id', item_controller.detail);
+router.post('/create', item_controller.create_post);
+router.post('/:id/delete', item_controller.delete_post);
+router.get('/:id/update', item_controller.update_get);
+router.post('/:id/update', item_controller.update_post);
+router.get('/:id', item_controller.detail);
 
 module.exports = router;
